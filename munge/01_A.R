8975fe7edf39a1889fd893674f2989410a2ffd45
Felipe González <felipexgonzalez@gmail.com>
2012-08-22 12:33:43 -0500

    Quitar 2000-2004 y script de manipulación y gráficas

--- munge/01_A.R
+++ munge/01_A.R
@@ -1,36 +1,45 @@
-# Melt de archivos
-archivos.nom <- grep("SIMAT", ls(), value = T)
-dat.l <- lapply(archivos.nom, function(nom){ print(nom)
-    dat <- eval( parse(text = nom) )
+## Nombres de tablas:
+archivos.nom <- paste("SIMAT", 2005:2009, sep = '.')
+## también se puede escribir:
+## archivos.nom <- c('SIMAT.2000','SIMAT.2001','SIMAT.2002')
+## y así sucesivamente.
+
+## Poner las tablas que nos intersan en una lista,
+## una vez que trasponemos mediciones y estaciones:
+dat.l <- lapply(archivos.nom, function(nom){ 
+	print(nom)
+    dat <- get(nom)
     dat.m <- melt(dat, id.vars = c("FECHA", "HORA"))
-    dat.m
+    codigos <- unique(dat.m$variable)
+    codigos.sep <- str_split_fixed(codigos, '_', n = 2)
+    estacion <- str_sub(codigos.sep[, 1], -3, -1)
+    medicion <- str_sub(codigos.sep[ ,1], -10, -4)
+    unidades <- codigos.sep[ , 2]
+    temp <- data.frame(variable = codigos,
+    	estacion = estacion,
+    	medicion = medicion,
+    	unidades = unidades )
+    join(dat.m, temp)
 })
+
+## Pegar los archivos en una sola tabla: Reduce es
+## recursivo.
+print('Pegar tablas')
 dat <- Reduce("rbind", dat.l)
 
-# Separar codigo en 3 variables
-etiquetas <- as.character(dat$variable)
-etiquetas.l <- strsplit(etiquetas ,'_') 
-# más lento:  etiquetas.l <- str_split(etiquetas, '_')
+print('Codificar valores faltantes')
+#value.2 <- as.numeric(car::recode(dat$value,
+#    "c(-9.9, -99, -99.9, -999, 'NR')= NA"))
+#dat$value <- value.2
+faltantes <- c('-9.9', '-99', '-99.9', '-999', 'NR')
+dat$value[dat$value %in% faltantes] <- NA
+dat$value <- as.numeric(dat$value)
 
-# Recodificar valores faltantes
-value.2 <- as.numeric(car::recode(dat$value,
-    "c(-9.9, -99, -99.9, -999, 'NR')= NA"))
-dat$value <- value.2
-
-
-
-dat$unidades <- sapply(etiquetas.l, '[', 2)
-dat$estacion <- str_sub( sapply(etiquetas.l, '[', 1), -3, -1)
-dat$medicion <- str_sub( sapply(etiquetas.l, '[', 1), -10, -4)
-dat$medicion.u <- paste(dat$medicion, dat$unidades, sep="_")
-dat$variable <- NULL
-
-# Fechas
-class(dat$FECHA)
-datos$fecha.hora <- strptime(paste(dat$FECHA, dat$HORA), format='%d/%m/%Y %H')
-dat$año <- year(datos$fecha.hora)
-dat$mes <- month(datos$fecha.hora)
-dat$dia <- wday(datos$fecha.hora) # Primer día de la semana es domingo
-dat$hora <- hour(datos$fecha.hora)
+print('Preparar fechas')
+dat$fecha.hora <- strptime(paste(dat$FECHA, dat$HORA - 1), format='%d/%m/%Y %H')
+dat$año <- year(dat$fecha.hora)
+dat$mes <- month(dat$fecha.hora, label = TRUE)
+dat$dia <- day(dat$fecha.hora) # Primer día de la semana es domingo
+dat$hora <- hour(dat$fecha.hora)
 dat$FECHA <- NULL
-cache("datos")
+cache('dat')
---
+++ src/tablas_graficas.R
@@ -0,0 +1,41 @@
+
+#' # Exploración de datos de SIMAT 2000-2009
+
+#' 
+head(dat)
+
+#' Análisis de datos faltantes por hora, año y estación. Obsérvese que hay más faltantes alrededor de la mitad del día en varias mediciones.
+
+#+ cache = T
+tab.faltantes <- ddply(dat, c('hora', 'estacion', 'medicion' ,'año'), 
+	summarise,
+	no.faltantes = sum(is.na(value)),
+	prop.faltantes = sum(is.na(value))/length(value))
+tab.faltantes.1 <- subset(tab.faltantes, 
+	medicion %in% c('NO2','NOX','O3','CO') )
+ggplot(tab.faltantes.1, 
+	aes(x=hora, y=prop.faltantes, colour= año, group=año)) +
+	geom_line() + facet_grid(estacion~medicion)
+
+#' Usar dcast para trasponer mediciones en columnas, como es más usual.
+dat.c <- dcast(dat, 
+	estacion + año + mes + dia + hora ~ medicion, value.var='value')
+#' Filtramos 4 meses y 3 horas del día para nuestro análisis.
+dat.c.sub <- subset(dat.c, 
+	hora %in% c(12,15,18) & mes %in% c('Jan','Mar','Jul','Oct'))
+
+#' Relación de temperatura y ozono en cada estación, mes y hora del día:
+
+#+ warning = F
+ggplot(dat.c.sub, aes(x = TMP, y = O3, colour=estacion)) + 
+	facet_grid(hora~mes) +
+	geom_point() + 
+	geom_smooth(method = 'loess')
+
+#' Podemos mejorar la gráfica haciendo modificaciones al *default* anterior:
+
+#+ warning = F
+ggplot(dat.c.sub, aes(x = TMP, y = O3, colour=estacion)) + 
+	facet_grid(hora~mes) +
+	geom_point(alpha=0.2, size=2) + 
+	geom_smooth(method='loess', span=1.2, size=0.7)
