6abed3e9a3fb88973ab4836fd15bd3d5abc26c50
Sonia <sonia@box.local>
2012-08-30 15:24:40 -0500

    Actualizacion de referencias y cambio en el nombre de variable

--- src/ejemplo_manipulacion.R
+++ src/ejemplo_manipulacion.R
@@ -5,19 +5,19 @@
 #' ## Lectura de datos y estructura
 #' Los datos se leen con la función **get** de **`ProjectTemplate`**.
 #+ getfunction
-dat <- get('SIMAT.2005')
+datos <- get('SIMAT.2005')
 
 #' Los datos están desorganizados: los nombres de las columnas incluyen información acerca de las unidades de medición (estaciones), de las mediciones y de las unidades de medición:
-head(dat)
+head(datos)
 
 #' Con la función **melt** de **`reshape2`** transponemos la base por mediciones y estaciones. Con esto podemos crear fácilmente identificadores de mediciones y estaciones y extraer las unidades de cada medición. Adicionalmente, podremos pegar de manera segura a las tablas de otros años.  
 #+ meltfunction
 library(reshape2)
-dat.m <- melt(dat, id.vars = c('FECHA', 'HORA'))
+dat.m <- melt(datos, id.vars = c('FECHA', 'HORA'))
 head(dat.m)
 
 #' Lo que ahora interesa es la columna '_variable_', cada nombre dice la medicion que se evalúa, la estación donde se evalúa y las unidades de la medición. En este caso los valores de la columna '_variable_' son los siguientes.
-# # tablecodigos, results='asis', echo = F
+# # tablecodigos, results='asis', echo = FALSE, message = FALSE
 # library(xtable)
 # codigos <- as.character(dat.m$variable)
 # tab.df <- data.frame(codigos)
@@ -30,12 +30,14 @@ head(dat.m)
 #' 
 #' ## Creación de variables
 #' Con la librería **`stringr`** se manipulan los códigos para separarlos en las tres variables que interesan. 
+#+ codigos
 library(stringr)
 codigos <- dat.m$variable
 
 #' * **str_split_fixed** 
 #' 
 #' Separa una cadena en distintas partes siguiendo un patrón, en este caso ('_'), y devuelve las nuevas cadenas en una matriz que además se puede definir la dimensión
+#+ str_split_fixed
 codigos.sep <- str_split_fixed(dat.m$variable, '_', n = 2)
 head(codigos.sep)
 
@@ -84,11 +86,13 @@ dat.m$value <- as.numeric(dat.m$value)
 #' URL [http://www.johnmyleswhite.com/notebook/2010/08/26/projecttemplate/](http://www.johnmyleswhite.com/notebook/2010/08/26/projecttemplate/)
 #' 2. Hadley Wickham. *Reshape2*.
 #' URL [http://had.co.nz/reshape/](http://had.co.nz/reshape/)
-#' 3. Hadley Wickham. *stringr: modern, consistent string processing*.
+#' 3. Hadley Wickham. *Reshaping Data with the reshape Package*.
+#' URL [http://www.jstatsoft.org/v21/i12/paper](http://www.jstatsoft.org/v21/i12/paper)
+#' 4. Hadley Wickham. *stringr: modern, consistent string processing*.
 #' URL [http://journal.r-project.org/archive/2010-2/RJournal_2010-2_Wickham.pdf](http://journal.r-project.org/archive/2010-2/RJournal_2010-2_Wickham.pdf)
-#' 4. Grolemund G, Wickham H. *Dates and Times Made Easy with lubridate*. Rice University.
+#' 5. Grolemund G, Wickham H. *Dates and Times Made Easy with lubridate*. Rice University.
+#' URL [http://www.jstatsoft.org/v40/i03/paper](http://www.jstatsoft.org/v40/i03/paper)
+#' 6. Hadley Wickham. *The Split-Apply-Combine Strategy for Data Analysis*.
 #' URL [http://www.jstatsoft.org/v40/i01/paper](http://www.jstatsoft.org/v40/i01/paper)
-#' 5. Hadley Wickham. *The Split-Apply-Combine Strategy for Data Analysis*
-#' URL [http://www.jstatsoft.org/v40/i01/paper](http://www.jstatsoft.org/v40/i01/paper)
-#' 6. Hadley Wickham. *Plyr*
+#' 7. Hadley Wickham. *Plyr*.
 #' URL [http://plyr.had.co.nz/](http://plyr.had.co.nz/)
